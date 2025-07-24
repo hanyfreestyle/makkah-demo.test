f808984824be00b77198acf23860869db8f327ba
Hany Darwish <hany.freestyle4u@gmail.com>
2025-07-24 11:07:45 +0300

    Update

--- resources/views/makkah/layouts/inc/header_menu_home.blade.php
+++ resources/views/makkah/layouts/inc/header_menu_home.blade.php
@@ -61,7 +61,7 @@
                   </li>
                   <li><a href="{{route('web.latest_news')}}">{{__('web/def.menu.latest_news')}}</a></li>
                   <li><a href="{{route('web.contact_us')}}">{{__('web/def.menu.contact_us')}}</a></li>
-                  <li class="special-link"><a href="tel:{{$webConfig->phone_call}}">{{$webConfig->phone_num}} </a></li>
+                  <li class="special-link cta_number"><a href="tel:{{$webConfig->phone_call}}">{{$webConfig->phone_num}} </a></li>
                 </ul>
               </div>
             </nav>
