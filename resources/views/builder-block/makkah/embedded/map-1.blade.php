16ee0fdf64a2514ed3afb9f91bf9ac4f0f58abe4
Hany Darwish <hany.freestyle4u@gmail.com>
2025-08-04 16:26:03 +0300

    Update

--- resources/views/builder-block/makkah/embedded/map-1.blade.php
+++ resources/views/builder-block/makkah/embedded/map-1.blade.php
@@ -4,8 +4,17 @@
 @endphp
 
 {{--{{getData($schema,'map_url')}}--}}
-<div class="google-map map-wrapper">
-  <div class="map-overlay"></div>
+<div class="map-wrapper">
+  <div class="map-overlay">
+    <h2>الموقع بالقرب من أبرز المناطق الحيوية</h2>
+    <ul>
+      <li>يبعد عن المنطقة الأولى مسافة قصيرة</li>
+      <li>يقع بالقرب من المركز التجاري</li>
+      <li>قريب من وسائل النقل العامة</li>
+      <li>يبعد دقائق عن المدارس والمستشفيات</li>
+    </ul>
+    <a href="#">اضغط هنا لعرض الخريطة</a>
+  </div>
   <iframe src="{{getData($schema,'map_url')}}&hl=en" width="100%" height="100%" style="border:0;" allowfullscreen=""  loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
 
@@ -22,16 +31,18 @@
           height: 75vh;
       }
 
+      .map-wrapper iframe{
+          z-index: 1;
+      }
+
       .map-wrapper .map-overlay {
           position: absolute;
           top: 0;
           left: 0;
           right: 0;
           bottom: 0;
-          /*background: transparent;*/
           z-index: 10;
-          /*background: yellow!important;*/
-          /* يمنع أي interaction مع الماوس */
+          background: rgba(0,0,0,.2);
       }
 
       .map-wrapper iframe {
